f2e2d0539d525eba78809a398537192622ee0af4
Ritvik Nagpal <ritviknagpal48@gmail.com>
2019-05-22 13:38:53 +0530

    Code for quick sort added in 'newbranch'

--- sort.cpp
+++ sort.cpp
@@ -2,6 +2,46 @@
 using namespace std;
 void quick_sort(int* arr, int n)
 {
+    if(n <= 1)
+    {
+        return;
+    }
+    int temp = arr[0];
+    int index = 0;
+    for(int i = 1; i < n; ++i)
+    {
+        if(arr[i] < temp)
+        {
+            index++;
+        }
+    }
+    int k = arr[index];
+    arr[index] = arr[0];
+    arr[0] = k;
+    int i = 0;
+    int j = n-1;
+    while(i <= index && j >= index)
+    {
+        if(arr[i] > arr[index] && arr[j] < arr[index])
+        {
+            int l = arr[i];
+            arr[i] = arr[j];
+            arr[j] = l;
+            i++;
+            j--;
+        }
+        else if(arr[i] > arr[index])
+        {
+            j--;
+        }
+        else if(arr[j] < arr[index])
+        {
+            i++;
+        }
+        
+    }
+    quick_sort(arr, index);
+    quick_sort(arr + index + 1, n - index - 1);
     
 }
 void merge_sort(int* arr, int n)
